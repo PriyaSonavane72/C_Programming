7f329f4a2dfdd0e0e5d3d8149d8720b7744dc7a2
PriyaSonavane72 <134388979+PriyaSonavane72@users.noreply.github.com>
2025-02-03 08:46:57 -0800

    Squre_Number

---
+++ Looping/01 To  Print Squre of Given Number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0,Sqr = 0;
+
+    printf("\n ===========================***=============================");
+
+    printf("\n Enter a Number : ");
+    scanf("%d",No);
+
+    Sqr = No * No;
+
+    printf("\n Square of Given Number %d is = %d." ,No, Sqr);
+
+    printf("\n\n===========================***==============================");
+
+    getch();
+    return 0;
+
+}
