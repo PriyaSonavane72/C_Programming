ce40d3ef18c519fdf15a8988482e77d9f4bd590b
PriyaSonavane72 <134388979+PriyaSonavane72@users.noreply.github.com>
2025-02-03 08:43:18 -0800

    Swaping_number Without Temp

---
+++ Basic_Program/02 Swaping of 2 Number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 0, No2 = 0;
+
+    printf("\n Enter Number 1 : ");
+    scanf("%d",&No1);
+    printf("\n Enter Number 2 : ");
+    scanf("%d",&No2);
+
+    printf("\n\n Given Numbers Before swap \n Number1 = %d, Number2 = %d. " ,No1,No2);
+
+    No1 = No1 + No2;
+    No2 = No1 - No2;
+    No1 = No1 - No2;
+
+    printf("\n\n Given Number After swap \n Number1 = %d, Number2 = %d.",No1,No2);
+
+    getch();
+    return 0;
+
+}
