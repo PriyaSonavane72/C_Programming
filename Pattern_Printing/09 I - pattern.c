435b2eb453bfceb5df664ad03bf275de8c3896f4
PriyaSonavane72 <134388979+PriyaSonavane72@users.noreply.github.com>
2025-03-14 11:30:18 -0700

    Print_I

---
+++ Pattern_Printing/09 I - pattern.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+     int R = 0,C = 0, X = 0;
+
+     printf("\n\n Enter The Row & column Size : ");
+     scanf("%d",&X);
+
+     printf("\n\n***********************************************\n\n");
+
+      for(R = 1; R <= X; R++)
+      {
+          for(C = 1; C <= X; C++)
+          {
+              if( R == 1 || R == X||  C ==( X/2 + 1))
+              {
+                  printf(" * ");
+              }
+              else
+              {
+                  printf("   ");
+              }
+          }
+          printf("\n");
+      }
+
+      printf("\n\n************************************************\n\n");
+
+      getch();
+      return 0;
+ }
+
