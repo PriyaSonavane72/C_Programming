a2246de8f77a4b81c12c418fdf1b1a68ccc39a48
Priya Sonavane <134388979+PriyaSonavane72@users.noreply.github.com>
2025-04-20 10:34:40 -0700

    Add files via upload

---
+++ Bitwise/LSF.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+     int No = 0,Ret = 0;
+
+    printf("\n Enter 1st Number ");
+    scanf("%d",&No);
+
+    Ret = No << 5;
+
+    printf("\n Value of No = %d", No);
+    printf("\n Value of Ret = %d", Ret);
+
+    printf("\n\n===================****=====================\n");
+
+    getch();
+    return 0;
+}
+
+
