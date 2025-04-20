2a0a863fabcc9ab82837fc01fe267fe1c11c302c
Priya Sonavane <134388979+PriyaSonavane72@users.noreply.github.com>
2025-04-20 10:46:10 -0700

    Add files via upload

---
+++ Bitwise/04 Bitwise Left shift Implimentation.c.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+     int No = 0,Res = 0;
+
+    printf("\n Enter 1st Number ");
+    scanf("%d",&No);
+
+    printf("\n\n===================****=====================\n");
+
+     Res = No << 4;
+
+    printf("\n Result => %d << 4 %d", No, Res);
+
+
+    printf("\n\n===================****=====================\n");
+
+    getch();
+    return 0;
+}
+
+
